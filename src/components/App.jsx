833031475dafae821a78680001a36b2c64d1b878
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-06-14 19:58:46 +0100

    update  state and update list array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputTextState] = useState("");
+  const [toDoItems, setToDoItems] = useState([]);
+
+  function handleEvent(event) {
+    const newValue = event.target.value;
+
+    setInputTextState(newValue);
+  }
+
+  function clickButton() {
+    setToDoItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleEvent} value={inputText} type="text" />
+        <button onClick={clickButton}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {toDoItems.map((item) => {
+            return <li>{item}</li>;
+          })}
         </ul>
       </div>
     </div>
